f94c6004e6bdc69c37c6398d315d1defb9a84314
iron-stomach <huhsoup126@gmail.com>
2026-04-24 13:55:27 +0900

    fix: set base path for GitHub Pages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,15 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../design-system/**/*.stories.@(ts|tsx)', '../stories/**/*.stories.@(ts|tsx)'],
+  stories: ['../design-system/**/*.stories.@(ts|tsx)'],
   addons: ['@storybook/addon-essentials'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
+  },
+  viteFinal: async (config) => {
+    config.base = '/soup/';
+    return config;
   },
 };
 
